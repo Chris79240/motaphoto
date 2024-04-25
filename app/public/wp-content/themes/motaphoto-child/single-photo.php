0f1e1a46fc4148969eb32f31e5616720f6a09687
Chris79240 <oservice.fr@gmail.com>
2024-04-25 23:11:34 +0200

    css single-photo

--- app/public/wp-content/themes/motaphoto-child/single-photo.php
+++ app/public/wp-content/themes/motaphoto-child/single-photo.php
@@ -15,28 +15,35 @@ if (have_posts()) : while (have_posts()) : the_post();
         $type = get_field('type');
         $annee = get_field('annee');
 ?>
-        <main id="content" <?php post_class('site-main'); ?> style="display: flex; justify-content: space-between;">
-            <div class="left-side" style="width: 48%;">
+        <main id="content" <?php post_class('site-main'); ?>>
+            <div class="left-side">
                 <header class="page-header">
-                    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+                    <?php the_title('<h2 class="entry-title">', '</h2>'); ?>
                     <p>Référence: <?php echo esc_html($reference); ?></p>
                     <p>Catégorie: <?php echo esc_html($categorie); ?></p>
                     <p>Format: <?php echo esc_html($format); ?></p>
                     <p>Type: <?php echo esc_html($type); ?></p>
                     <p>Année: <?php echo esc_html($annee); ?></p>
                 </header>
+
+                <hr class="header-divider" />
+
+                <div class="button-photo">
+                    <p>Cette photo vous intéresse-t-elle ?</p>
+                    <a href="<?php echo esc_url(get_permalink(get_page_by_path('contact'))); ?>" class="button">Contact</a>
+                </div>
             </div>
-            <div class="right-side" style="width: 48%;">
+            <div class="right-side">
                 <?php if (has_post_thumbnail()) : ?>
                     <div class="photo-main">
                         <?php the_post_thumbnail('large'); ?>
                     </div>
                 <?php endif; ?>
-                <div style="text-align: center; margin-top: 20px;">
-                    <p>Cette photo vous intéresse-t-elle ?</p>
-                    <a href="<?php echo esc_url(get_permalink(get_page_by_path('contact'))); ?>" class="button" style="background-color: #C4C4C4; padding: 10px 20px; color: #fff; text-decoration: none; display: inline-block;">Contact</a>
-                </div>
-                <div class="photo-navigation" style="text-align: center; margin-top: 20px;">
+
+
+
+
+                <div class="photo-navigation">
                     <button onclick="location.href='<?php echo get_previous_post_link(); ?>'">&#9664;</button>
                     <?php if (has_post_thumbnail()) : ?>
                         <div class="photo-thumbnail">
