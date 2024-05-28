fa083f180e7492caa90b41324988313b96a1e859
Chris79240 <oservice.fr@gmail.com>
2024-05-28 10:58:24 +0200

    Sass

--- app/public/wp-content/themes/motaphoto-child/functions.php
+++ app/public/wp-content/themes/motaphoto-child/functions.php
@@ -6,7 +6,7 @@
 function theme_enqueue_styles_and_scripts()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-theme-style', get_stylesheet_directory_uri() . '/css/theme.css', ['parent-style']);
+    wp_enqueue_style('child-theme-style', get_stylesheet_directory_uri() . '/sass/style.css', ['parent-style']);
     wp_enqueue_script('theme-script', get_stylesheet_directory_uri() . '/js/index.js', ['jquery'], null, true);
     wp_localize_script('theme-script', 'ajax_object', ['ajaxurl' => admin_url('admin-ajax.php')]);
 }
